54e73e568f2704da1ffdeac3b80c91b5496def34
sjkim <voidsjkim@gmail.com>
2025-08-22 00:08:16 +0900

    김승준

--- src/main/java/kr/co/bnksys/sangsang/model/RecommendRequest.java
+++ src/main/java/kr/co/bnksys/sangsang/model/RecommendRequest.java
@@ -1,23 +1,27 @@
 package kr.co.bnksys.sangsang.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.util.Map;
 
 public class RecommendRequest {
-    private Map<String, Integer> user_scores;
+
+    private Map<String, Number> user_scores;
     private Integer top_k;
 
     public RecommendRequest() {}
 
-    public RecommendRequest(Map<String, Integer> user_scores, Integer top_k) {
+    public RecommendRequest(Map<String, Number> user_scores, Integer top_k) {
         this.user_scores = user_scores;
         this.top_k = top_k;
     }
 
-    public Map<String, Integer> getUser_scores() {
+    public Map<String, Number> getUser_scores() {
         return user_scores;
     }
 
-    public void setUser_scores(Map<String, Integer> user_scores) {
+    public void setUser_scores(Map<String, Number> user_scores) {
         this.user_scores = user_scores;
     }
 
